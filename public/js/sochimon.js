5c90312e842d0fd20fb9d57c855c672ff4da9a87
Kris Sum <kris@switchsystems.co.uk>
2014-02-21 22:57:26 +0000

    updated js pathws

--- public/js/sochimon.js
+++ public/js/sochimon.js
@@ -1,5 +1,5 @@
 // create a map in the "map" div, set the view to a given place and zoom
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([51.505, -0.09], 4);
 var plottedPolyline;
 var currentMarkers = new Object();
 
@@ -8,38 +8,38 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 		attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-$('').click(function(){
-	id = $(this).data('')
-	getMarkerData(id);
-});
-
 function getMarkerData(e)
 {
 	e.preventDefault();
 	var country = $(e.target).data('country');
 	var detail = $('li.country-detail[data-country="'+country+'"]');
-	var id  = $(e.target).data('country-latLng');
 	$('#route').append(detail);
 	$('#route li[data-country="'+country+'"]').show();
 	$('#countries li[data-country="'+country+'"]').remove();
 
-	createMarkerFrom(id, imgSrc);
+	countryDiv = $('.countries').find("[data-country='" + country + "']");
+	var lat = $(countryDiv).data('country-lat');
+	var lng = $(countryDiv).data('country-Lng');
+
+	//var img = countryDiv.find('img');
+
+	createMarker(lat, lng, country);
 }
 
-function createMarker(latLng, imgSrc)
+function createMarker(lat, lng, country)
 {
-	customIcon = createCustomIcon(imgSrc);
-	popupHtml  = createPopupMarkup(obj);
-	marker = L.marker(latLng, {icon: customIcon}).addTo(map).bindPopup(popupHtml);
-	currentMarkers[obj.data.id] = marker;
-
+	//customIcon = createCustomIcon(imgSrc);
+	popupHtml  = createPopupMarkup(country);
+	marker = L.marker([lat, lng]).addTo(map);
+	currentMarkers[country] = marker;
+console.log(marker);
 	addMarkerToRoute();
 }
 
-function createPopupMarkup(obj) {
+function createPopupMarkup(country) {
 	popupHtml=document.createElement('DIV');
 
-	$(popupHtml).append(obj.data.name);
+	$(popupHtml).append(country);
 
 	return popupHtml;
 }
@@ -48,9 +48,9 @@ function addMarkerToRoute() {
 	plottedPolyline = L.Polyline.Plotter(currentMarkers,{weight: 5}).addTo(map);
 }
 
-function createCustomIcon(obj) {
+function createCustomIcon(imgSrc) {
 	var customIcon = L.icon({
-			iconUrl: obj.data.flag,
+			iconUrl: imgSrc,
 
 			iconSize:     [38, 95], // size of the icon
 			shadowSize:   [50, 64], // size of the shadow
