6786ebe0414b097bdcbcc531922cad709c247b3d
Jon Day <jondazed@googlemail.com>
2014-02-21 21:27:20 +0000

    added custom icon js to sochimon.js

--- public/js/sochimon.js
+++ public/js/sochimon.js
@@ -15,12 +15,13 @@ $('').click(function(){
 
 function getMarkerData(id)
 {
-	$.get('/api/XXX/' + id, createMarker, 'json').done(function(){});
+	$.get('/api/XXX/' + id, createMarkerFromJson, 'json').done(function(){});
 }
 
-function createMarker(obj, ajaxStatus)
+function createMarkerFromJson(obj, ajaxStatus)
 {
-	L.marker(obj.data.latLng).addTo(map)
+	customIcon = createCustomIcon(obj);
+	L.marker(obj.data.latLng, {icon: customIcon}).addTo(map)
 	
 	currentMarkers[obj.data.id] = obj.data.latLng;
 	
@@ -31,4 +32,17 @@ function addMarkerToRoute() {
 	plottedPolyline = L.Polyline.Plotter(currentMarkers,{weight: 5}).addTo(map);
 }
 
+function createCustomIcon(obj) {
+	var customIcon = L.icon({
+	    iconUrl: obj.data.flag,
 
+	    iconSize:     [38, 95], // size of the icon
+	    shadowSize:   [50, 64], // size of the shadow
+	    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+	    shadowAnchor: [4, 62],  // the same for the shadow
+	    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+	});
+	
+	
+	return customIcon;
+}
