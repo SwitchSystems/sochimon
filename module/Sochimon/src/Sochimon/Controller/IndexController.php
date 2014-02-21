ca10c8770e0f57bceae26683b9a6b631383f1f5f
Tom Ford <tom@switchsystems.co.uk>
2014-02-21 20:55:21 +0000

    removed kris merge break

--- module/Sochimon/src/Sochimon/Controller/IndexController.php
+++ module/Sochimon/src/Sochimon/Controller/IndexController.php
@@ -13,11 +13,6 @@ class IndexController extends AbstractActionController
 		$data = $this->getServiceLocator()->get('DataGrabber');
 		/*@var $data \Sochimon\Service\DataGrabber */
 
-<<<<<<< HEAD
-		var_dump($data->updateCountriesCache());exit;
-=======
->>>>>>> de65c2d5784ddfad5aab557eb3de45892912a2f9
-
 		$countries = $this->forward()->dispatch('Sochimon\Controller\CountriesController',['action' => 'index'])->setTerminal(true);
 		
 		
@@ -26,11 +21,6 @@ class IndexController extends AbstractActionController
 		return new ViewModel([
 				'countriesHTML' => $viewRender->render($countries),
 		]);
-	}
-
-	public function krisAction() {
-		echo 'hi';
-		return new ViewModel();
 	}
 
 }
