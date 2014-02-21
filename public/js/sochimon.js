9309daaaa4168b65826cf88780f47355682a45c4
Tom Ford <tom@switchsystems.co.uk>
2014-02-21 23:16:29 +0000

    fix default map zoom

--- public/js/sochimon.js
+++ public/js/sochimon.js
@@ -1,5 +1,5 @@
 // create a map in the "map" div, set the view to a given place and zoom
-var map = L.map('map').setView([51.505, -0.09], 4);
+var map = L.map('map').setView([51.505, -0.09], 2);
 var plottedPolyline;
 var currentMarkers = new Object();
 
